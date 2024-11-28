1b81efdf77d9b2c2c559798069ac7d1a52999221
abdu raheem <rahee9156@gmail.com>
2024-11-28 07:22:13 +0530

    sixth commit

--- rbac_project/urls.py
+++ rbac_project/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.permissions import AllowAny
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 schema_view = get_schema_view(
@@ -24,3 +26,5 @@ urlpatterns = [
     path('api/', include('rbac_app.urls')),
     path('', schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
